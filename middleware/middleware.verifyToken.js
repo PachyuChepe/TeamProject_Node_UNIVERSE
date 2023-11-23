291502de927b6448ce433d4f64f32048439f091f
rizzy <12hyojinkim14@gmail.com>
2023-11-23 17:50:50 +0900

    [UPDATE] 로그인,인증 코드 수정 passportlocal -> 숙련강의방식

--- middleware/middleware.verifyToken.js
+++ middleware/middleware.verifyToken.js
@@ -1,18 +1,32 @@
-// 로그인 검증 미들웨어
+const jwt = require('jsonwebtoken'); // jwt
 
-exports.isLoggedIn = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    next();
-  } else {
-    res.status(403).send("로그인이 필요한 기능입니다.");
+const dotenv = require('dotenv');
+dotenv.config();
+
+const db = require("../sequelize/models");
+const User = db.User;
+
+module.exports = async (req, res, next) => {
+  const { authorization } = req.headers;
+  const [authType, authToken] = (authorization || '').split(' ');
+
+  if (!authToken || authType !== 'Bearer') {
+    res.status(401).send({
+      errorMessage: '로그인 후 이용 가능한 기능입니다.',
+    });
+    return;
   }
-};
 
-exports.isNotLoggedIn = (req, res, next) => {
-  if (!req.isAuthenticated()) {
-    next();
-  } else {
-    // 이미 로그인 한 상태라면
-    res.status(400).send("이미 로그인한 상태입니다.");
+  try {
+    const { id } = jwt.verify(authToken, process.env.JWT_SECRET); // 암호화한 키 해독해 id 할당
+    const user = await User.findByPk(id); // 존재하는 id인 경우 user에 담김
+    res.locals.user = user; // 해당 id의 유저정보 res.locals에 담아 어디서든 호출 가능
+    next
+    ();
+  } catch (err) {
+    console.error(err);
+    res.status(401).send({
+      errorMessage: `로그인 후 이용 가능한 기능입니다.`,
+    });
   }
 };
